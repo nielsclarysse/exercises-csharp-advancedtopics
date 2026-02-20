942b65095c0c07e5719a482efb7d19631590913b
Niels Clarysse <niels.clarysse@student.howest.be>
2026-02-20 17:22:24 +0100

    EX1 - B

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -18,3 +18,27 @@ foreach (int i in Fibonacci(10))
 {
     //Console.WriteLine(i);
 }
+
+// ***********
+// * EX1 - B *
+// ***********
+
+static void Main()
+{
+    Console.WriteLine("Please enter a text: ");
+    string myText = Console.ReadLine().Trim().ToLower();
+
+    string myReversedText = string.Concat(Reverse(myText));
+
+    Console.WriteLine($"The text \"{myText}\" is {(myText == myReversedText ? "a" : "not a")} palindrome");
+}
+
+static IEnumerable<char> Reverse(string text)
+{
+    for (int i = text.Length - 1; i >= 0; i--)
+    {
+        yield return text[i];
+    }
+}
+
+//Main();
