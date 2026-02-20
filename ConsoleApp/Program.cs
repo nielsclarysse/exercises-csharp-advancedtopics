b239abeb29f1326f12a2ed452d3092c5c1ebb9b6
Niels Clarysse <niels.clarysse@student.howest.be>
2026-02-20 17:18:02 +0100

    EX1 - A

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿// ***********
+// * EX1 - A *
+// ***********
+
+IEnumerable<int> Fibonacci(int length)
+{
+    (int a, int b) = (0, 1);
+
+    for (int i = 0; i < length; i++)
+    {
+        yield return a;
+
+        (a, b) = (b, a + b);
+    }
+}
+
+foreach (int i in Fibonacci(10))
+{
+    //Console.WriteLine(i);
+}
